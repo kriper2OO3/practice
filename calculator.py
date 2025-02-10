db5ccc529a3a6863f27639882b13c23ee33145da
kriper2OO3 <sp10072OO3@mail.ru>
2025-02-10 05:30:11 +0500

    Первый коммит

---
+++ calculator.py
@@ -0,0 +1,13 @@
+def calc():
+    op = input("Выберите операцию (+, -, *, /): ")
+    a, b = float(input("Первое число: ")), float(input("Второе число: "))
+
+    if (op == '+'): res = a + b
+    elif (op == '-'): res = a - b
+    elif (op == '*'): res = a * b
+    elif (op == '/'): res = a / b
+    else: res = "Некорректная операция"
+    
+    return res
+
+print(calc())
