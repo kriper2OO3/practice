5f988f60dd26a789311e29e12369c5a301ea505d
kotipek <sepo10072003@mail.ru>
2025-03-17 13:31:08 +0500

    Добавлена обработка ошибки при делении на ноль

--- calculator.py
+++ calculator.py
@@ -5,7 +5,9 @@ def calc():
     if (op == '+'): res = a + b
     elif (op == '-'): res = a - b
     elif (op == '*'): res = a * b
-    elif (op == '/'): res = a / b
+    elif (op == '/'): 
+        if b == 0: res = "Ошибка: деление на ноль"
+        else: res = a / b
     else: res = "Некорректная операция"
     
     return res
